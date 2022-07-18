31e7c337859f6ceab30eefc34ea8c9c03dddc7eb
Vu Van Tai <vutai13196@gmail.com>
2022-07-18 10:31:27 +0700

    querystring

--- demo_querystring.js
+++ demo_querystring.js
@@ -5,5 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
+    console.log('>>> check req.url: ', req.url)
+    console.log('>>> check url.pares: ', q)
     res.end(txt);
 }).listen(8080);
